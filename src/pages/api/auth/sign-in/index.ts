72fde397f1e8be3b8abbcb10384d6991fb6af216
Carlos Pavajeau <cantte098@gmail.com>
2023-02-02 16:56:22 -0500

    feat: add admin emails verification before sing in

--- src/pages/api/auth/sign-in/index.ts
+++ src/pages/api/auth/sign-in/index.ts
@@ -8,9 +8,27 @@ const handler = async (
   const supabase = createServerSupabaseClient({ req, res })
 
   if (req.method === 'POST') {
+    const { email } = req.body.email
+
+    // Check if this email is an admin email
+    const { data: admin, error: adminError } = await supabase.from('admin_emails')
+      .select()
+      .eq('email', email)
+      .limit(1)
+
+    if (adminError !== null) {
+      res.status(500).json({ error: adminError.message })
+      return
+    }
+
+    if (admin === undefined || admin?.length === 0) {
+      res.status(500).json({ error: 'No eres administrador' })
+      return
+    }
+
     const { data, error } = await supabase.auth.signInWithPassword(req.body)
 
-    if (error != null) {
+    if (error !== null) {
       res.status(500).json({ error: error.message })
       return
     }

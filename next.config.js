42f2521012e7aeff10ef363aa1c48bd38b338025
Carlos Pavajeau <cantte098@gmail.com>
2023-02-06 16:53:16 -0500

    feat: add legal information

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  async rewrites () {
+    return [
+      {
+        source: '/legal/terms',
+        destination: '/html/terms-and-conditions.html'
+      },
+      {
+        source: '/legal/vehicle-leasing-terms',
+        destination: '/html/vehicle-leasing-contract.html'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig

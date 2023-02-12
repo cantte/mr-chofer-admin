e8f41c86adb3aa7c4635daf1a8143791968c11aa
Carlos Pavajeau <cantte098@gmail.com>
2023-02-11 22:23:04 -0500

    feat: add more legal pages

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
       {
         source: '/legal/vehicle-leasing-terms',
         destination: '/html/vehicle-leasing-contract.html'
+      },
+      {
+        source: '/legal/personal-data-privacy',
+        destination: '/html/personal-data-privacy.html'
+      },
+      {
+        source: '/legal/special-terms',
+        destination: '/html/special-terms.html'
       }
     ]
   }

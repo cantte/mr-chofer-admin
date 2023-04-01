1cf52efbb0d264fedf70ee042971d7edeb490cf5
Carlos Pavajeau <cantte098@gmail.com>
2023-04-01 11:10:06 -0500

    feat: include driver id

--- src/app/admin/drivers/[id]/page.tsx
+++ src/app/admin/drivers/[id]/page.tsx
@@ -150,6 +150,19 @@ const DriverPage: FC<Props> = ({ params }) => {
                 <dl>
                   <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                     <dt className='text-sm font-medium text-gray-500'>
+                      Cédula
+                    </dt>
+                    <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+                      {driver.id}
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+
+              <div className='border-t border-gray-200'>
+                <dl>
+                  <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+                    <dt className='text-sm font-medium text-gray-500'>
                       Nombre
                     </dt>
                     <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
